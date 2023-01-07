6526425ba49affab07c67257f8f7c9adb048f0e0
itibanbosi <51027729+itibanbosi@users.noreply.github.com>
2023-01-07 22:20:56 +0900

    Update pxt.json, main.blocks, eurekaV2.ts

--- eurekaV2.ts
+++ eurekaV2.ts
@@ -1,34 +1,28 @@
-/* i-o V2  20221216 */
-
+/* i-o V2  20230107 */
 
 enum onoff {
     ON,
     OFF,
 }
 
-
-enum neoLED_color {
-    White,
-    Red,
-    Yellow,
-    Green,
-    Blue,
-    Orange,
-    Indigo,
-    Violet,
-    Purple,
-    Black,
-}
-
-
 let io_neo = neopixel.create(DigitalPin.P9, 3, NeoPixelMode.RGB);
+io_neo.setBrightness(15)
+
+for (let n = 0; n < 3; n++) {
+    io_neo.setPixelColor(n, neopixel.colors(NeoPixelColors.Black))
+}
+io_neo.show()
 
 
-//% color="#ff4500" weight=94 block="iːo"
+//% color="#ff4500" weight=94 
+namespace iːo_V2 {
 
-namespace newio_blocks {
-
-
+    export enum BT {
+        //% block="lighter"
+        lighter,
+        //% block="darker"
+        darker
+    }
 
     //% color="#4741f1" weight=89 blockId=neopixel_blue block="Traffic light blue|%mode|" group="1 i:o Neopixel"
     export function neopixel_blue_block(mode: onoff) {
@@ -81,76 +75,38 @@ namespace newio_blocks {
         }
     }
 
-    //% color="#20b2aa" weight=82 blockId=neopixel_select block="Color LED(s)|%neo_color|,|%neo_number|,on" group="1 i:o Neopixel"
+    //% color.shadow="colorNumberPicker"
+    //% color="#20b2aa" weight=82 blockId=neopixel_select block="|%neo_number|LED(s) ON   color $color" group="1 i:o Neopixel"
     //% neo_number.min=0 neo_number.max=3
-    export function neopixel_select_block(neo_color: neoLED_color, neo_number: number) {
+    export function neopixel_select_block(neo_number: number, color: number) {
         for (let n = 0; n < 2; n++) {
             io_neo.setPixelColor(n, neopixel.colors(NeoPixelColors.Black))
         }
         io_neo.show()
-        switch (neo_color) {
-            case neoLED_color.Red:
-                for (let n = 0; n < neo_number; n++) {
-                    io_neo.setPixelColor(n, neopixel.colors(NeoPixelColors.Red))
-                }
-                io_neo.show()
-                break;
-            case neoLED_color.Orange:
-                for (let n = 0; n < neo_number; n++) {
-                    io_neo.setPixelColor(n, neopixel.colors(NeoPixelColors.Orange))
-                }
-                io_neo.show()
-                break;
-            case neoLED_color.Yellow:
-                for (let n = 0; n < neo_number; n++) {
-                    io_neo.setPixelColor(n, neopixel.colors(NeoPixelColors.Yellow))
-                }
-                io_neo.show()
-                break;
-            case neoLED_color.Green:
-                for (let n = 0; n < neo_number; n++) {
-                    io_neo.setPixelColor(n, neopixel.colors(NeoPixelColors.Green))
-                }
-                io_neo.show()
-                break;
-            case neoLED_color.Blue:
-                for (let n = 0; n < neo_number; n++) {
-                    io_neo.setPixelColor(n, neopixel.colors(NeoPixelColors.Blue))
-                }
-                io_neo.show()
-                break;
-            case neoLED_color.Indigo:
-                for (let n = 0; n < neo_number; n++) {
-                    io_neo.setPixelColor(n, neopixel.colors(NeoPixelColors.Indigo))
-                }
-                io_neo.show()
-                break;
-            case neoLED_color.Violet:
-                for (let n = 0; n < neo_number; n++) {
-                    io_neo.setPixelColor(n, neopixel.colors(NeoPixelColors.Violet))
-                }
-                io_neo.show()
-                break;
-            case neoLED_color.Purple:
-                for (let n = 0; n < neo_number; n++) {
-                    io_neo.setPixelColor(n, neopixel.colors(NeoPixelColors.Purple))
-                }
-                io_neo.show()
-                break;
-            case neoLED_color.White:
-                for (let n = 0; n < neo_number; n++) {
-                    io_neo.setPixelColor(n, neopixel.colors(NeoPixelColors.White))
-                }
-                io_neo.show()
-                break;
-            case neoLED_color.Black:
-                for (let n = 0; n < neo_number; n++) {
-                    io_neo.setPixelColor(n, neopixel.colors(NeoPixelColors.Black))
-                }
-                io_neo.show()
-                break;
+        for (let n = 0; n < neo_number; n++) {
+            io_neo.setPixelColor(n, color)
         }
+        io_neo.show()
+
     }
+
+    //% color.shadow="colorNumberPicker"
+    //% color="#20b2aa" weight=82 blockId=neopixel_set block="No.|%neo_number|LED ON  color $color" group="1 i:o Neopixel"
+    //% neo_number.min=1 neo_number.max=3 neo_number.defl=1
+    export function neopixel_set_block(neo_number: number, color: number) {
+        io_neo.setPixelColor(neo_number - 1, color)
+        io_neo.show()
+    }
+
+    //% color="#696969" weight=82 blockId=neopixel_Brightness block="Color LED(s) Brightness|%neo_Brightness| " group="1 i:o Neopixel"
+    //% neo_Brightness.min=0 neo_Brightness.max=255 neo_Brightness.defl=15
+    export function neopixel_Brightness_block(neo_Brightness: number) {
+        io_neo.setBrightness(neo_Brightness)
+    }
+
+
+
+
     //% color="#9400d3" weight=81 blockId=neopixel_reinbow block="RGB on" group="1 i:o Neopixel"
     export function neopixel_rainbow() {
         io_neo.showRainbow(1, 180)
@@ -194,18 +150,26 @@ namespace newio_blocks {
 
     //% color="#a0522d"  weight=79 blockId=IO_human_DISP block="Show human detecting sensor binary" group="2 i:o Human detecting sensor"
     export function IO_human_DISP() {
-
         pins.setPull(DigitalPin.P14, PinPullMode.PullNone);
         basic.showNumber(pins.digitalReadPin(DigitalPin.P14));
     }
 
-    //% color="#009A00"  weight=81 blockId=microbit2_decideLight block="Light sensor value darker than|%limit|" group="3 micro:bit light sensor"
+    //% color="#009A00"  weight=81 blockId=microbit2_decideLight block="Light sensor value |%brightness| than|%limit|" group="3 micro:bit light sensor"
     //% limit.min=0 limit.max=100
-    export function microbit2_decideLight(limit: number): boolean {
-        if (input.lightLevel() / 254 * 100 < limit) {
-            return true;
-        } else {
-            return false;
+    export function microbit2_decideLight(Brightness: BT, limit: number): boolean {
+        switch (Brightness) {
+            case BT.darker:
+                if (input.lightLevel() / 254 * 100 <= limit) {
+                    return true;
+                } else {
+                    return false;
+                }
+            case BT.lighter:
+                if (input.lightLevel() / 254 * 100 < limit) {
+                    return false;
+                } else {
+                    return true;
+                }
         }
     }
 
@@ -213,9 +177,7 @@ namespace newio_blocks {
 
     //% color="#009A00"  weight=80 blockId=microbit2_denkitemp block="Light sensor value" group="3 micro:bit light sensor"
     export function microbit2_denkitemp(): number {
-
         return Math.round(input.lightLevel() / 254 * 100);
-
     }
 
 
@@ -225,7 +187,7 @@ namespace newio_blocks {
     }
 
 
-    //% color="#696969" weight=58 blockId=IO_relay block="Relay (digital) write|%mode|" group="4 i:o Relay control"
+    //% color="#696969" weight=58 blockId=IO_relay block="Relay (digital) write|%mode|" group="4 Relay control"
     export function IO_relay(mode: onoff) {
         switch (mode) {
             case onoff.ON: {
@@ -236,11 +198,16 @@ namespace newio_blocks {
             }
         }
     }
-    //% color="#696969" weight=56 blockId=IO_relay_2 block="Relay (analog) write|%syuturyoku|" group="4 i:o Relay control"
+
+    //% color="#696969" weight=56 blockId=IO_relay_2 block="Relay (analog) write|%syuturyoku|" group="4 Relay control"
     //% syuturyoku.min=0 syuturyoku.max=1023
     export function IO_relay_2(syuturyoku: number) {
         return pins.analogWritePin(AnalogPin.P8, syuturyoku);
     }
-}
 
-// ここにコードを追加します
+    //% color="#ff7b00" weight=32 blockId=eureka_tempDS block="DS18B20 Temp" group="5 Temp sensor"
+    //% advanced=true
+    export function eureka_tempDS(): number {
+        return Math.round(dstemp.celsius(DigitalPin.P0) * 10) / 10
+    }
+}
